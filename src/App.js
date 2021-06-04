fb6e18f225643276d29303398940e6314a522eb9
Talia <tshaviv33@gmail.com>
2021-06-04 16:32:22 +0300

    Added API integration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
+import { useState } from "react";
 import "./App.css";
 import WeatherInfo from "./components/WeatherInfo";
 import Footer from "./components/Footer";
 
 export default function App() {
-  return (
-    <div className="App container d-flex flex-column justify-content-center align-items-center vh-100">
-      <WeatherInfo />
-      <Footer />
-    </div>
-  );
+  const defaultCity = "tel aviv";
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleData(data) {
+    setWeatherData({
+      ready: true,
+      city: data.name,
+      date: data.dt,
+      feelsLike: Math.round(data.main.feels_like),
+      humidity: Math.round(data.main.humidity),
+      temp: Math.round(data.main.temp),
+      tempMax: Math.round(data.main.temp_max),
+      tempMin: Math.round(data.main.temp_min),
+      description: data.weather[0].description,
+      icon: data.weather[0].icon,
+      wind: Math.round(data.wind.speed),
+      lat: data.coord.lat,
+      lon: data.coord.lon,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="App container d-flex flex-column justify-content-center align-items-center vh-100">
+        <WeatherInfo weatherData={weatherData} />
+        <Footer />
+      </div>
+    );
+  } else {
+    const apiKey = "edcd663668b8087c96e88fbd0856ea83";
+    const weatherApiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${defaultCity}&appid=${apiKey}&units=metric `;
+
+    fetch(weatherApiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        handleData(data);
+      });
+    return (
+      <div className="App container d-flex flex-column justify-content-center align-items-center vh-100">
+        Loading...
+        <Footer />
+      </div>
+    );
+  }
 }
--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -1,34 +1,53 @@
 import CitySearch from "./CitySearch";
 import "./WeatherInfo.css";
 
-export default function WeatherInfo() {
+export default function WeatherInfo(props) {
+  const {
+    city,
+    date,
+    description,
+    feelsLike,
+    humidity,
+    temp,
+    tempMax,
+    tempMin,
+    icon,
+    wind,
+  } = props.weatherData;
+
   return (
     <div className="WeatherInfo w-100 shadow-lg rounded p-4 pt-2">
       <CitySearch />
-      <h1>New York</h1>
+      <h1>{city}</h1>
       <ul className="list-unstyled">
         <li>
           Wednesday <span className="result">07:00</span>
         </li>
-        <li>Mostly Cloudy</li>
+        <li className="text-capitalize">{description}</li>
       </ul>
       <div className="d-flex justify-content-around align-items-center">
         <div className="d-flex align-items-center">
           <i className="fas fa-sun weather-icon me-2"></i>
-          <h2 className="ms-1">
-            6°<span className="units ms-1">C | F</span>
-          </h2>
+          <div className="d-flex flex-column justify-content-center">
+            <h2 className="ms-1">
+              {temp}°<span className="units ms-1">C | F</span>
+            </h2>
+            <p className="high-low-temps">
+              High: <span className="result me-2">{tempMax}°</span> Low:{" "}
+              <span className="result">{tempMin}°</span>
+            </p>
+          </div>
         </div>
         <div>
           <ul className="list-unstyled">
             <li>
-              Feels Like: <span className="result">6°</span>
+              Feels Like: <span className="result">{feelsLike}°</span>
             </li>
             <li>
-              Humidity: <span className="result">72%</span>
+              Humidity: <span className="result">{humidity}%</span>
             </li>
             <li>
-              Wind: <span className="result">13 km/h</span>
+              Wind: <span className="result">{wind} km/h</span>
             </li>
           </ul>
         </div>
